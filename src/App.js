dfc40cc7567b37fc3e6d08a5d6dc603ff6872e70
Donghee Lee <jacobdhlee@gmail.com>
2017-02-17 12:58:54 -0500

    add signup form using native-base and animation for singup form

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
   Dimensions,
   LayoutAnimation,
 } from 'react-native';
-
+import { Content, Form, Item, Input, Button } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 // import { StackNavigator } from 'react-navigation'
 // import Animation from 'lottie-react-native';
@@ -21,8 +21,10 @@ class App extends Component {
     super()
     this.state = {
       toggle: false,
+      open: false,
     }
 
+    this.animatedValue = new Animated.Value(0)
     this.animateValue1 = new Animated.Value(0)
     this.animateValue2 = new Animated.Value(0)
     this.animateValue3 = new Animated.Value(0)
@@ -30,10 +32,17 @@ class App extends Component {
     this.backAnimation = this.backAnimation.bind(this);
     this.animated = this.animated.bind(this);
     this.expandComponent = this.expandComponent.bind(this);
+    this.signUpAnimation = this.signUpAnimation.bind(this);
   }
 
   componentWillMount() {
+    LayoutAnimation.spring();
+    // LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     this.animated()
+  }
+
+  componentWillUpdate() {
+    this.signUpAnimation()
   }
 
   animated() {
@@ -86,6 +95,15 @@ class App extends Component {
     }
   }
 
+  signUpAnimation() {
+    Animated.timing(this.animatedValue, {
+      toValue: 1,
+      duration: 2000,
+    }).start()
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+  }
+
   expandComponent() {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     this.setState({toggle: !this.state.toggle});
@@ -112,35 +130,37 @@ class App extends Component {
         </Animated.View>
 
         <Animated.View style={[{ right: signupButton }, { opacity: this.animateValue3}]}>
-          <TouchableOpacity style={styles.button} onPress={(form) => this.backAnimation()}>
+          <TouchableOpacity style={styles.button} onPress={() => this.setState({open: !this.state.open})}>
             <Text style={styles.text}>Sign up</Text>
           </TouchableOpacity>
         </Animated.View>
       </View>
     )
 
-    const signupForm = (
-      <View>
-      </View>
+    const signUpForm = (
+      <Animated.View style={[styles.signupBox, {opacity: this.animatedValue}]}>
+        <Content contentContainerStyle={{marginBottom:5}}>
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item>
+              <Input placeholder="Password" />
+            </Item>
+            <Item>
+              <Input placeholder="Confirm Password" />
+            </Item>
+          </Form>
+        </Content>
+      </Animated.View>
     )
+
     return (
       <View style={styles.container}>
         <Animated.View style={[styles.animationContainer, { opacity: this.animateValue1 }]}>
           <Text style={{fontSize: 25, fontWeight: '700'}}>Animation Login</Text>
         </Animated.View>
-        <View style={styles.buttonContainer}>
-          <Animated.View style={[{ left: loginButton }, { opacity: this.animateValue2}]}>
-            <TouchableOpacity style={styles.button} onPress={() => this.backAnimation('login')}>
-              <Text style={styles.text}>Log in</Text>
-            </TouchableOpacity>
-          </Animated.View>
-
-          <Animated.View style={[{ right: signupButton }, { opacity: this.animateValue3}]}>
-            <TouchableOpacity style={styles.button} onPress={(form) => this.backAnimation()}>
-              <Text style={styles.text}>Sign up</Text>
-            </TouchableOpacity>
-          </Animated.View>
-        </View>
+        {this.state.open ? signUpForm : logAndSignButton}
       </View>
     )
   }
@@ -163,6 +183,17 @@ const styles = StyleSheet.create({
     flex: 4,
     alignItems: 'center',
     padding: 20,
+  },
+  signupBox: {
+    flex: 17,
+    padding: 20,
+  },
+  signupButton: {
+    margin: 5,
+  },
+  signupButtonText: {
+    fontSize: 16,
+    fontWeight: '600'
   },
   button: {
     width: width * 0.8, 
